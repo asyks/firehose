0b64fe303dcd2f7b3e060998a3360e448d964b43
asyks <aaron@aaronsykes.com>
2022-04-23 21:23:04 -0700

    Add connection send test

--- tests/test_connection_async.py
+++ tests/test_connection_async.py
@@ -4,6 +4,7 @@ from unittest.mock import AsyncMock, Mock, patch
 import pytest
 
 from spigot.connection import Connection
+from spigot.constants import DEFAULT_ENCODING
 
 
 @pytest.mark.asyncio
@@ -20,3 +21,31 @@ class TestConnectionOpen:
         await connection.open()
 
         assert mock_asyncio_open_connection.call_count == 1
+
+
+@pytest.mark.asyncio
+@patch("spigot.connection.open_connection", return_type=AsyncMock)
+class TestConnectionSend:
+    async def test_sends_str_data_and_collects_response(
+        self, mock_asyncio_open_connection: AsyncMock,
+    ) -> None:
+        response_data = 'some arbirary response data'
+        mock_stream_reader = AsyncMock(
+            spec=StreamReader,
+            readline=AsyncMock(
+                side_effect=[
+                    response_data.encode(DEFAULT_ENCODING), ''
+                ]
+            )
+        )
+
+        mock_asyncio_open_connection.return_value = (
+            mock_stream_reader, Mock(spec=StreamWriter)
+        )
+        connection = Connection(hostname="foobar", port=80)
+
+        await connection.open()
+
+        response = await connection.send('some arbitrary request data')
+
+        assert response.data.read() == response_data
